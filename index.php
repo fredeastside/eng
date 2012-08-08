31fcc9125abff32e04572d75cc0bb43673f91a6b
Сергей <fredrsf@fredrsf.(none)>
2012-08-08 19:00:36 +0400

    new

--- index.php
+++ index.php
@@ -28,9 +28,8 @@ switch ($request->action){
 		break;
 }
 
-//printAr($_REQUEST);
 $modul = new fmakeSiteModule();
-//printAr($_COOKIE);
+
 $url = $request -> getEscape('url') ? $request -> getEscape('url') : $request -> getEscape('modul');
 $url = explode('/', $url);
 $url = $url[0];
@@ -43,30 +42,11 @@ $globalTemplateParam->set('menu',$menu);
 $globalTemplateParam->set('url',$url);
 $globalTemplateParam->set('modul',$modul);
 
-$news_obj = new fmakeNews();
-$news = $news_obj->getNews(true);
-$news_url = $news_obj->getUrlNews();
-$globalTemplateParam->set('news_url',$news_url);
-$globalTemplateParam->set('news',$news);
-
-$incidents_obj = new fmakeIncidents();
-$incidents = $incidents_obj->getIncidents();
-$incident_url = $incidents_obj->getUrlIncident();
-$globalTemplateParam->set('incident_url',$incident_url);
-$globalTemplateParam->set('incidents',$incidents);
-
-$meets_obj = new fmakeMeets();
-$meets = $meets_obj->getMeets(true, 0, 2);
-$meet_url = $meets_obj->getUrlMeet();
-$globalTemplateParam->set('meet_url',$meet_url);
-$globalTemplateParam->set('meets',$meets);
-
 $modul->template = "base/main.tpl";
 
 if($modul->file){
 	include($modul->file.".php");
 } 
-
 
 $template = $twig->loadTemplate($modul->template);
 $template->display($globalTemplateParam->get());
